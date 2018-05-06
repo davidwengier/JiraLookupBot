09b7aed184c1dc910123656b8ce80abda15e1a3d
David Wengier <git@wengier.com>
2018-05-06 16:48:46 +1000

    Initial commit

---
+++ Function.cs
@@ -0,0 +1,51 @@
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Bot.Builder.Azure;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+
+namespace JiraLookupBot
+{
+    public static class Function
+    {
+        [FunctionName("Function")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        {
+            using (BotService.Initialize())
+            {
+                // Deserialize the incoming activity
+                string jsonContent = await req.Content.ReadAsStringAsync();
+                var activity = JsonConvert.DeserializeObject<Activity>(jsonContent);
+                // authenticate incoming request and add activity.ServiceUrl to MicrosoftAppCredentials.TrustedHostNames
+                // if request is authenticated
+                if (!await BotService.Authenticator.TryAuthenticateAsync(req, new[] { activity }, CancellationToken.None))
+                {
+                    return BotAuthenticator.GenerateUnauthorizedResponse(req);
+                }
+
+                if (activity != null)
+                {
+                    // one of these will have an interface and process it
+                    switch (activity.GetActivityType())
+                    {
+                        case ActivityTypes.Message:
+                            await Conversation.SendAsync(activity, () => new JiraDialog());
+                            break;
+
+                        default:
+                            log.Error($"Unknown activity type ignored: {activity.GetActivityType()}");
+                            break;
+                    }
+                }
+                return req.CreateResponse(HttpStatusCode.Accepted);
+            }
+        }
+    }
+}
---
+++ JiraDialog.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Atlassian.Jira;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace JiraLookupBot
+{
+    internal class JiraDialog : IDialog<object>
+    {
+        private const string Pattern = @"((?<!([A-Za-z]{1,10})-?)[A-Z]+-\d+)";
+
+        private readonly string ServerUrl = ConfigurationManager.AppSettings["JiraUrl"];
+        private readonly string JiraUserName = ConfigurationManager.AppSettings["JiraUserName"];
+        private readonly string JiraPassword = ConfigurationManager.AppSettings["JiraPassword"];
+
+        public Task StartAsync(IDialogContext context)
+        {
+            try
+            {
+                context.Wait(MessageReceivedAsync);
+            }
+            catch (OperationCanceledException error)
+            {
+                return Task.FromCanceled(error.CancellationToken);
+            }
+            catch (Exception error)
+            {
+                return Task.FromException(error);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            Jira j = Jira.CreateRestClient(ServerUrl, JiraUserName, JiraPassword);
+            var message = await argument;
+            foreach (Match match in Regex.Matches(message.Text, Pattern))
+            {
+                var issue = await j.Issues.GetIssueAsync(match.Value);
+
+                var c = new HeroCard(issue.Key.Value, issue.Summary, "Status: " + issue.Status.Name + ", Assignee: " + issue.Assignee + ", Resolution: " + issue.Resolution.Name, null, new List<CardAction>
+                {
+                    new CardAction("openUrl", "Open " + issue.Key.Value, value: new Uri(new Uri(ServerUrl), "/browse/" + issue.Key.Value))
+               });
+                var msg = context.MakeMessage();
+                msg.Attachments.Add(c.ToAttachment());
+                await context.PostAsync(message);
+            }
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
